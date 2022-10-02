5aedc1acec5e21eeb764cb2a8ab3d25d21f7f998
Colin Ford <colinford24@gmail.com>
2022-10-02 15:47:27 -0400

    added hello gradescope file to git

---
+++ assignment_0/hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Colin Ford
+# OSU Email: fordc2@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0
+# Due Date: 10/3/22
+# Description: First Assignment
+
+
+def gradescope_test():
+    return 42
